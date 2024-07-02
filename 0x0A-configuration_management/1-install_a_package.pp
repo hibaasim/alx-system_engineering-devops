233387d8ac1334ee4dc985b30e6882cef812ef87
hibaasim <hiba.asim10@gmail.com>
2024-07-02 02:38:29 -0700

    fix-2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # Manifest to install flask from pip3
 
-package { 'flask':
+package { 'python3.8':
+  ensure   => '3.8.10',
+  provider => 'pip3',
+}
+
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
@@ -8,5 +13,5 @@ package { 'flask':
 package { 'Werkzeug':
   ensure   => '2.1.1',
   provider => 'pip3',
-  require  => Package['flask'],
+  require  => Package['Flask'],
 }
